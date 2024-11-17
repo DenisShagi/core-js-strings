c346ae30944a86b079984e0e89109914f29520ee
Denis <denis.shagim@icloud.com>
2024-11-17 14:02:36 +0500

    34 passing test

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -375,8 +375,13 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const arr = str.split(' ');
+  const arr1 = [];
+  arr.map((value) => {
+    return arr1.push(value.split('').reverse().join(''));
+  });
+  return arr1.join(' ');
 }
 
 /**
